09b686a0c1ce075a584f99d82a13ed2b25bc5b4c
Matt Hammond <mjhammond94@gmail.com>
2023-04-27 21:16:40 +0100

    require paths added - app file updated and tested

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
 require_relative 'lib/database_connection'
+require_relative 'lib/post_repository'
+require_relative 'lib/user_repository'
 
 DatabaseConnection.connect('social_network')
 
+repo = PostRepository.new 
+selected = repo.find(4)
+puts selected.title
