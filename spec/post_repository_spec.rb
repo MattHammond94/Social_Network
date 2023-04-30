dcae46dc2225ba3f5a32181fa01a342ff371b7f9
Matt Hammond <mjhammond94@gmail.com>
2023-04-30 16:33:14 +0100

    Files cleaned and updated

--- spec/post_repository_spec.rb
+++ spec/post_repository_spec.rb
@@ -1,75 +1,75 @@
-# require 'post_repository'
+require 'post_repository'
 
-# describe PostRepository do
-#   def reset_post_table
-#     seed_sql = File.read('spec/social_network_seeds.sql')
-#     connection = PG.connect({ host: '127.0.0.1', dbname: 'social_network_test' })
-#     connection.exec(seed_sql)
-#   end
+describe PostRepository do
+  def reset_post_table
+    seed_sql = File.read('spec/social_network_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'social_network_test' })
+    connection.exec(seed_sql)
+  end
 
-#   before(:each) do
-#     reset_post_table
-#   end
+  before(:each) do
+    reset_post_table
+  end
 
-#   it 'Should return all post table rows as an array ' do
-#     repo = PostRepository.new
-#     all = repo.all
-#     expect(all.length).to eq 8
-#     expect(all.first.content).to eq 'I ran out of mana so no more fire balls'
-#     expect(all.last.views).to eq '4'
-#   end
+  it 'Should return all post table rows as an array ' do
+    repo = PostRepository.new
+    all = repo.all
+    expect(all.length).to eq 8
+    expect(all.first.content).to eq 'I ran out of mana so no more fire balls'
+    expect(all.last.views).to eq '4'
+  end
 
-#   it 'Should return the values of for the method calls on the selected row' do
-#     repo = PostRepository.new
-#     post = repo.find(2)
-#     expect(post.title).to eq 'Had another bad day'
-#     expect(post.content).to eq 'Took a bath - water extinguishes fire. What was I thinking?'
-#     expect(post.id).to eq '2'
-#   end
+  it 'Should return the values of for the method calls on the selected row' do
+    repo = PostRepository.new
+    post = repo.find(2)
+    expect(post.title).to eq 'Had another bad day'
+    expect(post.content).to eq 'Took a bath - water extinguishes fire. What was I thinking?'
+    expect(post.id).to eq '2'
+  end
 
-#   it 'Should create a new post row from a post obj' do
-#     repo = PostRepository.new
-#     post_1 = double :post, title: 'A greeting', content: 'Hello', views: 0, user_id: 1
-#     repo.create(post_1)
-#     expect(repo.all.last.title).to eq 'A greeting'
-#     expect(repo.all.last.content).to eq 'Hello'
-#     expect(repo.all.length).to eq 9
-#     expect(repo.all.last.user_id).to eq '1'
-#   end
+  it 'Should create a new post row from a post obj' do
+    repo = PostRepository.new
+    post_1 = double :post, title: 'A greeting', content: 'Hello', views: 0, user_id: 1
+    repo.create(post_1)
+    expect(repo.all.last.title).to eq 'A greeting'
+    expect(repo.all.last.content).to eq 'Hello'
+    expect(repo.all.length).to eq 9
+    expect(repo.all.last.user_id).to eq '1'
+  end
 
-#   it 'Should remove the row from DB that corresponds with id passed to delete method as arg' do
-#     repo = PostRepository.new
-#     repo.delete(1)
-#     repo.delete(3)
-#     repo.delete(4)
-#     expect(repo.all.length).to eq 5
-#   end
+  it 'Should remove the row from DB that corresponds with id passed to delete method as arg' do
+    repo = PostRepository.new
+    repo.delete(1)
+    repo.delete(3)
+    repo.delete(4)
+    expect(repo.all.length).to eq 5
+  end
 
-#   it 'Should update the selected post' do
-#     repo = PostRepository.new
-#     selected = repo.find(4)
-#     selected.title = "Sandwich Update"
-#     selected.content = "This is a new sandwich update"
-#     selected.views = 100
-#     selected.user_id = 1
-#     repo.update(selected)
-#     updated = repo.find(4)
-#     expect(updated.title).to eq "Sandwich Update"
-#     expect(updated.content).to eq "This is a new sandwich update"
-#     expect(updated.views).to eq '100'
-#     expect(updated.user_id).to eq '1'
-#   end
+  it 'Should update the selected post' do
+    repo = PostRepository.new
+    selected = repo.find(4)
+    selected.title = "Sandwich Update"
+    selected.content = "This is a new sandwich update"
+    selected.views = 100
+    selected.user_id = 1
+    repo.update(selected)
+    updated = repo.find(4)
+    expect(updated.title).to eq "Sandwich Update"
+    expect(updated.content).to eq "This is a new sandwich update"
+    expect(updated.views).to eq '100'
+    expect(updated.user_id).to eq '1'
+  end
 
-#   it 'Should only update specific values' do
-#     repo = PostRepository.new
-#     selected = repo.find(2)
-#     selected.title = "Updated title"
-#     selected.user_id = 3
-#     repo.update(selected)
-#     updated = repo.find(2)
-#     expect(updated.title).to eq "Updated title"
-#     expect(updated.content).to eq "Took a bath - water extinguishes fire. What was I thinking?"
-#     expect(updated.views).to eq '4'
-#     expect(updated.user_id).to eq '3'
-#   end
-# end
+  it 'Should only update specific values' do
+    repo = PostRepository.new
+    selected = repo.find(2)
+    selected.title = "Updated title"
+    selected.user_id = 3
+    repo.update(selected)
+    updated = repo.find(2)
+    expect(updated.title).to eq "Updated title"
+    expect(updated.content).to eq "Took a bath - water extinguishes fire. What was I thinking?"
+    expect(updated.views).to eq '4'
+    expect(updated.user_id).to eq '3'
+  end
+end
--- spec/user_repository_spec.rb
+++ spec/user_repository_spec.rb
@@ -1,65 +1,65 @@
-# require 'user_repository'
+require 'user_repository'
 
-# describe UserRepository do
+describe UserRepository do
 
-#   def reset_users_table
-#     seed_sql = File.read('spec/user_seeds.sql')
-#     connection = PG.connect({ host: '127.0.0.1', dbname: 'social_network_test' })
-#     connection.exec(seed_sql)
-#   end
+  def reset_users_table
+    seed_sql = File.read('spec/user_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'social_network_test' })
+    connection.exec(seed_sql)
+  end
 
-#   before(:each) do
-#     reset_users_table
-#   end
+  before(:each) do
+    reset_users_table
+  end
 
-#   it 'Should return an array of all rows within users' do
-#     repo = UserRepository.new
-#     users = repo.all 
-#     expect(users.length).to eq 4
-#     expect(users.first.id).to eq '1'
-#     expect(users.first.email_address).to eq 'obsidian_fire_mage69@gmail.com'
-#     expect(users[2].username).to eq 'BigKlopp'
-#   end
+  it 'Should return an array of all rows within users' do
+    repo = UserRepository.new
+    users = repo.all 
+    expect(users.length).to eq 4
+    expect(users.first.id).to eq '1'
+    expect(users.first.email_address).to eq 'obsidian_fire_mage69@gmail.com'
+    expect(users[2].username).to eq 'BigKlopp'
+  end
 
-#   it 'Should return the selected row' do
-#     repo = UserRepository.new 
-#     user = repo.find(2)
-#     expect(user.username).to eq 'Mr.Worldwide'
-#   end
+  it 'Should return the selected row' do
+    repo = UserRepository.new 
+    user = repo.find(2)
+    expect(user.username).to eq 'Mr.Worldwide'
+  end
 
-#   it 'Should return the selected row' do
-#     repo = UserRepository.new 
-#     user = repo.find(3)
-#     expect(user.email_address).to eq 'gengenpressed_to_perfection@tisacli.net'
-#     expect(user.id).to eq '3'
-#   end
+  it 'Should return the selected row' do
+    repo = UserRepository.new 
+    user = repo.find(3)
+    expect(user.email_address).to eq 'gengenpressed_to_perfection@tisacli.net'
+    expect(user.id).to eq '3'
+  end
 
-#   it 'Should take a user object as an arg and add it to the DB as a row' do
-#     repo = UserRepository.new
-#     user_1 = double :user, email_address: 'mattymoomilk@tiscali.net', username: 'MattyMooMilk'
-#     repo.create(user_1)
-#     expect(repo.all.last.email_address).to eq 'mattymoomilk@tiscali.net'
-#     expect(repo.all.last.username).to eq 'MattyMooMilk'
-#     expect(repo.all.length).to eq 5
-#     expect(repo.all.last.id).to eq '5'
-#   end
+  it 'Should take a user object as an arg and add it to the DB as a row' do
+    repo = UserRepository.new
+    user_1 = double :user, email_address: 'mattymoomilk@tiscali.net', username: 'MattyMooMilk'
+    repo.create(user_1)
+    expect(repo.all.last.email_address).to eq 'mattymoomilk@tiscali.net'
+    expect(repo.all.last.username).to eq 'MattyMooMilk'
+    expect(repo.all.length).to eq 5
+    expect(repo.all.last.id).to eq '5'
+  end
 
-#   it 'Should remove the row from DB that corresponds with id passed to delete as arg' do
-#     repo = UserRepository.new
-#     repo.delete(1)
-#     repo.delete(3)
-#     repo.delete(4)
-#     expect(repo.all.length).to eq 1
-#   end
+  it 'Should remove the row from DB that corresponds with id passed to delete as arg' do
+    repo = UserRepository.new
+    repo.delete(1)
+    repo.delete(3)
+    repo.delete(4)
+    expect(repo.all.length).to eq 1
+  end
 
-#   it 'Should correctly update the selected row when update method is called' do
-#     repo = UserRepository.new
-#     user_1 = repo.find(1)
-#     user_1.email_address = 'updatedemailaddress@gmail.com'
-#     user_1.username = 'updatedusername'
-#     repo.update(user_1)
-#     updated = repo.find(1)
-#     expect(updated.email_address).to eq 'updatedemailaddress@gmail.com'
-#     expect(updated.username).to eq 'updatedusername'
-#   end
-# end
+  it 'Should correctly update the selected row when update method is called' do
+    repo = UserRepository.new
+    user_1 = repo.find(1)
+    user_1.email_address = 'updatedemailaddress@gmail.com'
+    user_1.username = 'updatedusername'
+    repo.update(user_1)
+    updated = repo.find(1)
+    expect(updated.email_address).to eq 'updatedemailaddress@gmail.com'
+    expect(updated.username).to eq 'updatedusername'
+  end
+end
